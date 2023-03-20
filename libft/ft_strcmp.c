da100f5954d8a4bd66e4a35f072c3bfbf0ec3173
ahammout <issamhammoutan2@gmail.com>
2023-03-20 13:48:18 +0000

    Update libft

--- libft/ft_strcmp.c
+++ libft/ft_strcmp.c
@@ -1,19 +1,27 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_isprint.c                                       :+:      :+:    :+:   */
+/*   ft_strcmp.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: ahammout <ahammout@student.42.fr>          +#+  +:+       +#+        */
+/*   By: zessadqu <zessadqu@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2021/11/04 13:14:38 by ahammout          #+#    #+#             */
-/*   Updated: 2023/03/16 20:39:57 by ahammout         ###   ########.fr       */
+/*   Created: 2023/03/16 18:53:27 by zessadqu          #+#    #+#             */
+/*   Updated: 2023/03/16 18:53:50 by zessadqu         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "libft.h"
 
-int	ft_isprint(int p)
+int	ft_strcmp(const char *s1, const char *s2)
 {
-	if (p >= 33 && p <= 126)
-		return (1);
-	return (0);
+	size_t	i;
+
+	i = 0;
+	while (s1[i] == s2[i])
+	{
+		if (s1[i] == '\0' && s2[i] == '\0')
+			return (0);
+		i++;
+	}
+	return ((unsigned char)s1[i] - (unsigned char)s2[i]);
 }
--- libft/libft.h
+++ libft/libft.h
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   libft.h                                            :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: ahammout <marvin@42.fr>                    +#+  +:+       +#+        */
+/*   By: ahammout <ahammout@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/04 12:55:29 by ahammout          #+#    #+#             */
-/*   Updated: 2021/11/20 16:02:16 by ahammout         ###   ########.fr       */
+/*   Updated: 2023/03/20 00:10:57 by ahammout         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -36,7 +36,8 @@ char	*ft_strchr(const char *s, int c);
 size_t	ft_strlcat(char *dst, const char *src, size_t dstsize);
 size_t	ft_strlcpy(char *dst, const char *src, size_t len);
 int		ft_strncmp(const char *s1, const char *s2, size_t n);
-char	*ft_strnstr(const char *haystack, const char *needle, size_t len);
+int     ft_strcmp(const char *s1, const char *s2);
+char    *ft_strnstr(const char *haystack, const char *needle, size_t len);
 char	*ft_strrchr(const char *s, int c);
 int		ft_atoi(const char *str);
 void	*ft_calloc(size_t count, size_t size);
